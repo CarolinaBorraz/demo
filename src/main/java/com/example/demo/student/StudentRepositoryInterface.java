d2ccf77c6ef3119a0341ffee118cd291adb39eb2
Carolina Borraz <carolinaborrazr@gmail.com>
2023-04-25 22:07:47 -0600

    added a comment in repository

--- src/main/java/com/example/demo/student/StudentRepositoryInterface.java
+++ src/main/java/com/example/demo/student/StudentRepositoryInterface.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface StudentRepositoryInterface extends JpaRepository<Student, Long>{
     
+    // updates name and email of a student
     @Modifying
     @Query("UPDATE students SET name = ?2, email = ?3 WHERE id = ?1")
     public void editNameAndEmail(Long id, String name, String email);
